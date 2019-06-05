741608ef7a1a5e7b5fdcfca95dae97db712b28f7
linzebin <738780265@qq.com>
2019-06-05 21:15:55 +0800

    First commit test project

---
+++ src/main/java/com/lzb/githubtest/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.lzb.githubtest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+    @RequestMapping("name")
+    @ResponseBody
+    public String name()
+    {
+        return "hhhh";
+    }
+}
